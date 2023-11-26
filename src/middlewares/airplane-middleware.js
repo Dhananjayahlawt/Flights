e4005ca3cbdcb717d24378f9498bd135ba12c65a
Dhananjay Ahlawat <ahlawt.dhananjay@gmail.com>
2023-11-26 22:24:27 +0530

    feat:Added Seeders for createAirplane Services

--- src/middlewares/airplane-middleware.js
+++ src/middlewares/airplane-middleware.js
@@ -1,14 +1,21 @@
 const { StatusCodes } = require("http-status-codes");
 const { SuccessResponse, ErrorResponse } = require("../utils/common");
+const { AppError } = require("../utils/error");
 const ValidateCreateAirplane = function (req, res, next) {
   if (!req.body.modelNumber) {
     ErrorResponse.message = "Something Went wrong WHile Creating Airplane";
-    ErrorResponse.error =["Model Number not found in Incoming Request"] 
+    ErrorResponse.error = new AppError(
+      ["Model Number not found in Incoming Request"],
+      StatusCodes.BAD_REQUEST
+    );
     return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
   }
   if (!req.body.capacity) {
     ErrorResponse.message = "Something Went wrong WHile Creating Airplane";
-    ErrorResponse.error =["Capcity not found in Incoming Request"] 
+    ErrorResponse.error = new AppError(
+      ["Capcity not found in Incoming Request"],
+      StatusCodes.BAD_REQUEST
+    );
     return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
   }
   next();
