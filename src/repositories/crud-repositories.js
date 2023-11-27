d4b45962b2b641190ad0d3eb9e473ba9303c4b12
Dhananjay Ahlawat <ahlawt.dhananjay@gmail.com>
2023-11-27 13:08:14 +0530

    refactor:Remove TRy catch from crud repositry

--- src/repositories/crud-repositories.js
+++ src/repositories/crud-repositories.js
@@ -5,63 +5,37 @@ class CrudRepository {
   }
 
   async create(data) {
-    try {
-
-      const response = await this.model.create(data)
-      return response;
-    } catch (error) {
-      logger.error("Something wnet wrong in crud:Create");
-      throw error;
-    }
+    const response = await this.model.create(data);
+    return response;
   }
 
   async destroy(data) {
-    try {
-      const response = await this.model.destroy({
-        where: {
-          id: data,
-        },
-      });
-      return response;
-    } catch (error) {
-      logger.error("Something wnet wrong in crud:Destroy");
-      throw error;
-    }
+    const response = await this.model.destroy({
+      where: {
+        id: data,
+      },
+    });
+    return response;
   }
 
   async get(data) {
-    try {
-      const response = await this.model.findByPk(data);
-      return response;
-    } catch (error) {
-      logger.error("Something wnet wrong in crud:get");
-      throw error;
-    }
+    const response = await this.model.findByPk(data);
+    return response;
   }
 
   async getAll(data) {
-    try {
-      const response = await this.model.findAll();
-      return response;
-    } catch (error) {
-      logger.error("Something wnet wrong in crud:findAll");
-      throw error;
-    }
+    const response = await this.model.findAll();
+    return response;
   }
 
   async update(id, data) {
-    try {
-      const response = await this.model.update(data, {
-        where: {
-          id: id,
-        },
-      });
-      return response;
-    } catch (error) {
-      logger.error("Something wnet wrong in crud:update");
-      throw error;
-    }
+    const response = await this.model.update(data, {
+      where: {
+        id: id,
+      },
+    });
+    return response;
   }
 }
 
-module.exports=CrudRepository;
+module.exports = CrudRepository;
